654e9ab14b8116f2baac0a295b293db9e1e7d39a
Trevor Wang <oumei.vip@gmail.com>
2014-08-17 17:13:47 +0800

    Initial Commit

---
+++ Ddemo/MingDocument.h
@@ -0,0 +1,13 @@
+//
+//  MingDocument.h
+//  Ddemo
+//
+//  Created by Ming Wang on 8/17/14.
+//  Copyright (c) 2014 Ming Wang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MingDocument : NSDocument
+
+@end
---
+++ DdemoTests/DdemoTests.m
@@ -0,0 +1,34 @@
+//
+//  DdemoTests.m
+//  DdemoTests
+//
+//  Created by Ming Wang on 8/17/14.
+//  Copyright (c) 2014 Ming Wang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DdemoTests : XCTestCase
+
+@end
+
+@implementation DdemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
